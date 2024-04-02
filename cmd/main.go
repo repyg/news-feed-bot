cb3f431947c0bd23ee6fe975bc38d4a53574dd76
vladrepyj <vlad.kushchnetsov@bk.ru>
2024-04-02 18:48:02 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+
+	"github.com/vladrepyj/news-feed-bot/internal/bot"
+	"github.com/vladrepyj/news-feed-bot/internal/bot/middleware"
+	"github.com/vladrepyj/news-feed-bot/internal/botkit"
+	"github.com/vladrepyj/news-feed-bot/internal/config"
+	"github.com/vladrepyj/news-feed-bot/internal/fetcher"
+	"github.com/vladrepyj/news-feed-bot/internal/notifier"
+	"github.com/vladrepyj/news-feed-bot/internal/storage"
+	"github.com/vladrepyj/news-feed-bot/internal/summary"
+)
+
+func main() {
+	botAPI, err := tgbotapi.NewBotAPI(config.Get().TelegramBotToken)
+	if err != nil {
+		log.Printf("[ERROR] failed to create botAPI: %v", err)
+		return
+	}
+
+	db, err := sqlx.Connect("postgres", config.Get().DatabaseDSN)
+	if err != nil {
+		log.Printf("[ERROR] failed to connect to db: %v", err)
+		return
+	}
+	defer db.Close()
+
+	var (
+		articleStorage = storage.NewArticleStorage(db)
+		sourceStorage  = storage.NewSourceStorage(db)
+		fetcher        = fetcher.New(
+			articleStorage,
+			sourceStorage,
+			config.Get().FetchInterval,
+			config.Get().FilterKeywords,
+		)
+		summarizer = summary.NewOpenAISummarizer(
+			config.Get().OpenAIKey,
+			config.Get().OpenAIModel,
+			config.Get().OpenAIPrompt,
+		)
+		notifier = notifier.New(
+			articleStorage,
+			summarizer,
+			botAPI,
+			config.Get().NotificationInterval,
+			2*config.Get().FetchInterval,
+			config.Get().TelegramChannelID,
+		)
+	)
+
+	newsBot := botkit.New(botAPI)
+	newsBot.RegisterCmdView(
+		"addsource",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdAddSource(sourceStorage),
+		),
+	)
+	newsBot.RegisterCmdView(
+		"setpriority",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdSetPriority(sourceStorage),
+		),
+	)
+	newsBot.RegisterCmdView(
+		"getsource",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdGetSource(sourceStorage),
+		),
+	)
+	newsBot.RegisterCmdView(
+		"listsources",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdListSource(sourceStorage),
+		),
+	)
+	newsBot.RegisterCmdView(
+		"deletesource",
+		middleware.AdminsOnly(
+			config.Get().TelegramChannelID,
+			bot.ViewCmdDeleteSource(sourceStorage),
+		),
+	)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	go func(ctx context.Context) {
+		if err := fetcher.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run fetcher: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] fetcher stopped")
+		}
+	}(ctx)
+
+	go func(ctx context.Context) {
+		if err := notifier.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run notifier: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] notifier stopped")
+		}
+	}(ctx)
+
+	go func(ctx context.Context) {
+		if err := http.ListenAndServe("9.0.0.0:8080", mux); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run http server: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] http server stopped")
+		}
+	}(ctx)
+
+	if err := newsBot.Run(ctx); err != nil {
+		log.Printf("[ERROR] failed to run botkit: %v", err)
+	}
+}
---
+++ internal/fetcher/fetcher.go
@@ -0,0 +1,137 @@
+package fetcher
+
+import (
+	"context"
+	"log"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/tomakado/containers/set"
+
+	"github.com/vladrepyj/news-feed-bot/internal/model"
+	src "github.com/vladrepyj/news-feed-bot/internal/source"
+)
+
+type ArticleStorage interface {
+	Store(ctx context.Context, article model.Article) error
+}
+
+type SourcesProvider interface {
+	Sources(ctx context.Context) ([]model.Source, error)
+}
+
+type Source interface {
+	ID() int64
+	Name() string
+	Fetch(ctx context.Context) ([]model.Item, error)
+}
+
+type Fetcher struct {
+	articles ArticleStorage
+	sources  SourcesProvider
+
+	fetchInterval  time.Duration
+	filterKeywords []string
+}
+
+func New(
+	articleStorage ArticleStorage,
+	sourcesProvider SourcesProvider,
+	fetchInterval time.Duration,
+	filterKeywords []string,
+) *Fetcher {
+	return &Fetcher{
+		articles:       articleStorage,
+		sources:        sourcesProvider,
+		fetchInterval:  fetchInterval,
+		filterKeywords: filterKeywords,
+	}
+}
+
+func (f *Fetcher) Start(ctx context.Context) error {
+	ticker := time.NewTicker(f.fetchInterval)
+	defer ticker.Stop()
+
+	if err := f.Fetch(ctx); err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			if err := f.Fetch(ctx); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (f *Fetcher) Fetch(ctx context.Context) error {
+	sources, err := f.sources.Sources(ctx)
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+
+	for _, source := range sources {
+		wg.Add(1)
+
+		go func(source Source) {
+			defer wg.Done()
+
+			items, err := source.Fetch(ctx)
+			if err != nil {
+				log.Printf("[ERROR] failed to fetch items from source %q: %v", source.Name(), err)
+				return
+			}
+
+			if err := f.processItems(ctx, source, items); err != nil {
+				log.Printf("[ERROR] failed to process items from source %q: %v", source.Name(), err)
+				return
+			}
+		}(src.NewRSSSourceFromModel(source))
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+func (f *Fetcher) processItems(ctx context.Context, source Source, items []model.Item) error {
+	for _, item := range items {
+		item.Date = item.Date.UTC()
+
+		if f.itemShouldBeSkipped(item) {
+			log.Printf("[INFO] item %q (%s) from source %q should be skipped", item.Title, item.Link, source.Name())
+			continue
+		}
+
+		if err := f.articles.Store(ctx, model.Article{
+			SourceID:    source.ID(),
+			Title:       item.Title,
+			Link:        item.Link,
+			Summary:     item.Summary,
+			PublishedAt: item.Date,
+		}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (f *Fetcher) itemShouldBeSkipped(item model.Item) bool {
+	categoriesSet := set.New(item.Categories...)
+
+	for _, keyword := range f.filterKeywords {
+		if categoriesSet.Contains(keyword) || strings.Contains(strings.ToLower(item.Title), keyword) {
+			return true
+		}
+	}
+
+	return false
+}
